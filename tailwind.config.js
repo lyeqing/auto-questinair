be829dd4ea40153ec0b6863f6faa6272c935ae09
Louis <lyeqing@ymail.com>
2022-03-17 17:00:51 +1030

    added 2 pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    darkMode: false, // or 'media' or 'class'
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         extend: {},
     },
